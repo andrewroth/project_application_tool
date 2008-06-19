6727dc987d2506a91135ab67b86dd650aba06b3b
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-06-19 18:40:51 +0000

    added support for dev site

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,28 @@
-set :application, "Project Application Tool"
-set :repository,  "https://svn.ministryapp.com/pat/trunk"
-
-ssh_options[:port] = 40022
-set :user, "andrew"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/pat.ministryapp.com"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+puts ENV['target']
 
 role :app, "ministryapp.com"
 role :web, "ministryapp.com"
 role :db,  "ministryapp.com", :primary => true
 
+ssh_options[:port] = 40022
+set :user, "andrew"
+
+set :application, "Project Application Tool"
+
+if ENV['target'] == 'dev'
+  set :repository,  "https://svn.ministryapp.com/pat/trunk"
+  set :deploy_to, "/var/www/dev.pat.ministryapp.com"
+else
+  set :repository,  "https://svn.ministryapp.com/pat/trunk"
+  set :deploy_to, "/var/www/pat.ministryapp.com"
+end
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
 desc "Restart the web server"
 deploy.task :restart, :roles => :app do
-  sudo "/opt/lsws/bin/lswsctrl restart"
+  # sudo "/opt/lsws/bin/lswsctrl restart"
+  run "touch #{current_path}/tmp/restart.txt"
 end
