1753f637c57274e683f0a9b6f75bed9d9a712433
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-07-28 02:40:09 +0000

    added check for user nil

--- app/controllers/notification_acknowledgments_controller.rb
+++ app/controllers/notification_acknowledgments_controller.rb
@@ -1,5 +1,10 @@
 class NotificationAcknowledgmentsController < ApplicationController
   def create
+    if @user.nil?
+      render :inline => 'Need to login to remember hidden files'
+      return
+    end
+
     @notification = Notification.find params[:notification_id]
     @ack = NotificationAcknowledgment.new :viewer_id => @user.id,
              :notification_id => @notification.id
