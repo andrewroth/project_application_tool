f9f53094932fdb197ac3504c3641c0fdcf486936
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-11-05 02:16:21 +0000

    added phone number to registrant report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -943,6 +943,7 @@ class ReportsController < ApplicationController
       :pref1, 'string',
       :pref2, 'string',
       :accepted, 'string',
+      :phone, 'string',
       :email, 'string'
     ].flatten.compact )
     @rows = [ ]
@@ -977,7 +978,7 @@ class ReportsController < ApplicationController
       registrant = Registrant.new(p.person_lname, p.person_fname, gender,
                                   (if !@eg.has_your_campuses then :skip elsif p.campuses[0] then p.campuses[0].campus_shortDesc else '' end), 
                                   (if !@eg.has_your_campuses then :skip elsif p.campuses[0] then p.person_year.year_in_school.year_desc else '' end),
-      status, p1, p2, acceptance, p.person_email)
+      status, p1, p2, acceptance, p.person_local_phone, p.person_email)
       
       if block_given?
         yield v, @a, registrant
