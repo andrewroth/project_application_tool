8e30c0d8d797147c7e26fd80cacec711e44a13f5
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-11-11 18:56:49 +0000

    added check for svn, ruby; switches to installer.rb

--- installer/install.sh
+++ installer/install.sh
@@ -1,2 +1,24 @@
-# first things first, get ruby installed
-aptitude install ruby
+echo ""
+echo "This is the Project Application Tool (PAT) install script."
+echo " located at http://static.ministryapp.com/canada/pat_auto/install.sh"
+echo ""
+echo -n "Continue? (y/n) "
+read answer
+
+if [ $answer == "y" ]
+then
+
+  # install required packages
+  echo "First making sure ruby and subversion are installed.."
+  echo ""
+  apt-get install ruby subversion
+
+  # check out everything locally
+  echo "Checking out installer code"
+  echo ""
+  svn co https://svn.ministryapp.com/pat/trunk/installer pat_installer
+
+  # now switch to the ruby installer
+  ruby pat_installer/install.rb
+fi
+
