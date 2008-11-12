eb87f4a540802f207e8090321e7f540169a06386
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-11-12 22:21:01 +0000

    added a chmod g+rw so that other people can edit the files if necessary; added recursive flag

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,6 +59,7 @@ end
 unless ENV['target'] == 'demo'
   deploy.task :after_symlink do
     run "cp #{File.join(deploy_to, 'database.yml')} #{File.join(current_path, 'config', 'database.yml')}"
-    run "chown www-data.www-data #{current_path}"
+    run "chown www-data.www-data #{current_path} -R"
+    run "chmod g+rw #{current_path} -R"
   end
 end
