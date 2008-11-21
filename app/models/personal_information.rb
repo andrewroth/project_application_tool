ad11c39b9c29bdacb4504561aca6647c58d1edf9
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2008-11-21 20:35:39 +0000

    #1560 defaulted dropdowns to blank or unknown

--- app/models/personal_information.rb
+++ app/models/personal_information.rb
@@ -14,6 +14,15 @@ class PersonalInformation < CustomElement
     # options, it messes up the update_attributes save since it's a defined
     # method and not a column
     if params[:appln_person]
+      # copy local address info to permanent if requested
+      copy = params[:appln_person].delete :permanent_same_as_local
+      if copy
+        for suffix in %w(city addr pc phone)
+	  params[:appln_person][:"person_#{suffix}"] = params[:appln_person][:"person_local_#{suffix}"]
+        end
+        params[:appln_person][:province_id] = params[:appln_person][:person_local_province_id]
+      end
+
       person_params = params[:appln_person].clone
       person_params.delete 'email'
       person.update_attributes(person_params)
