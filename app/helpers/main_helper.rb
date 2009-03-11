44b06d1d29e1eedee57064e2808d60b8367e806d
evan.chan <evan.chan@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2009-03-11 20:29:22 +0000

    profile notes

---
+++ app/helpers/main_helper.rb
@@ -0,0 +1,68 @@
+module MainHelper
+  def link(title, path)
+    "<a href='#{path}'>#{title}</a>"
+  end
+
+  def view_entire_link(profile_id)
+    link 'view entire', "/acceptance/view_entire?profile_id=#{profile_id}"
+  end
+
+  def view_summary_link(profile_id)
+    link 'view summary', "/acceptance/view_summary?profile_id=#{profile_id}"
+  end
+
+  def edit_always_editable_link(profile_id)
+    link 'edit', "/appln/view_always_editable/?profile_id=#{profile_id}"
+  end
+
+  def manual_donations_link(motv_code)
+    link 'manual donations', "/manual_donations/new?motv_code=#{motv_code}"
+  end
+
+  def costing_link(profile_id)
+    link 'costing', "/optin_cost_items/index?profile_id=#{profile_id}"
+  end
+
+  def travel_link(profile_id)
+    link 'travel', "/profile_travel_segments/index?profile_id=#{profile_id}"
+  end
+  
+  def profile_notes_link(profile_id)
+    link 'profile notes', "/profile_notes/?profile_id=#{profile_id}"
+  end
+
+  def evaluate_link(profile_id, project_id)
+    evaluate_with_message 'evaluate', profile_id, project_id
+  end
+
+  def continue_evaluate_link(msg, profile_id, project_id)
+    evaluate_with_message msg, profile_id, project_id
+  end
+
+  def evaluate_with_message(msg, profile_id, project_id)
+    link msg, "/processor/evaluate?profile_id=#{profile_id}?project_id=#{project_id}"
+  end
+
+  def view_entire_processor_link(profile_id, project_id)
+    link 'view entire', "/processor/view_entire?profile_id=#{profile_id}"
+  end
+
+  def view_entire_processor_link(profile_id)
+    link 'view entire', "/processor/view_entire?profile_id=#{profile_id}"
+  end
+  
+  def withdraw_move_link(id, label = 'move/withdraw..')
+     link label, "/profiles/move/#{id}"
+  end
+  
+  def ref(ref) 
+    if ref.nil?
+      "none"
+    elsif ref.status != 'completed' && ref.mail?
+      @user.can_evaluate? ? "mailing (" + link("input", ref.ref_url) + ")" : "mailing"
+    else
+      ref.status
+    end
+  end
+  
+end
