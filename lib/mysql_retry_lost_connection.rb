ebcb4484fb4780320231bb02d1a55d7b68bfb4ad
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2009-03-24 17:02:39 +0000

    update mysql_retry_lost_connection to production

--- lib/mysql_retry_lost_connection.rb
+++ lib/mysql_retry_lost_connection.rb
@@ -1,23 +1,40 @@
 module ActiveRecord
   module ConnectionAdapters
     class MysqlAdapter
-      def execute(sql, name = nil) #:nodoc:
-        reconnect_lost_connections = true
+      RETRY_COUNT = 3
+
+      # retry lost connection / server gone away errors
+      def execute_with_handle_connection_errors(sql, name = nil) #:nodoc:
+        retries = RETRY_COUNT
         begin
-          log(sql, name) { @connection.query(sql) }
+          execute_without_handle_connection_errors sql, name
         rescue ActiveRecord::StatementInvalid => exception
-          RAILS_DEFAULT_LOGGER.info("ActiveRecord::StatementInvalid Error: #{exception.message}\n#{exception.backtrace.join("\n")}")
-          if reconnect_lost_connections and exception.message =~ /(Lost connection to MySQL server during query|MySQL server has gone away)/
-            reconnect_lost_connections = false
+          RAILS_DEFAULT_LOGGER.info("MysqlAdapter FAIL: execute got StatementInvalid.  Retries left: #{retries}")
+          if retries > 0 and exception.message =~ /(Lost connection to MySQL server during query|MySQL server has gone away)/
             reconnect!
+            retries -= 1
             retry
-          elsif exception.message.split(":").first =~ /Packets out of order/
-            raise ActiveRecord::StatementInvalid, "'Packets out of order' error was received from the database. Please update your mysql bindings (gem install mysql) and read http://dev.mysql.com/doc/mysql/en/password-hashing.html for more information.  If you're on Windows, use the Instant Rails installer to get the updated mysql bindings."
-          else
-            raise
           end
         end
       end
+      alias_method_chain :execute, :handle_connection_errors
+
+      # retry column methods that get nil back from execute
+      def columns_with_handle_executions(table_name, name = nil) #:nodoc:
+        retries = RETRY_COUNT
+        begin
+          columns_without_handle_executions table_name, name
+        rescue NoMethodError => exception
+          RAILS_DEFAULT_LOGGER.info("MysqlAdapter FAIL: column NoMethodError.  Retries left: #{retries}")
+          if retries > 0 and  exception.message =~ /(You have a nil object when you didn't expect it!)/
+            reconnect!
+            retries -= 1
+            retry
+          end
+        end
+      end
+      alias_method_chain :columns, :handle_executions
+
     end
   end
 end
