e5f0fa886ec03d5050f91dd83bcd0b90a565cf3f
andrew <andrew@fe61012b-fd52-4b1a-a1fc-0ef6296cdb1a>
2009-04-01 03:21:54 +0000

    updated our config to be like a new generated rails config

--- config/environment.rb
+++ config/environment.rb
@@ -19,18 +19,35 @@ end
 # questionnaire engine config
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
   
-  # Skip frameworks you're not going to use (only works if using vendor/rails)
-  # config.frameworks -= [ :action_web_service, :action_mailer ]
-
   # Add additional load paths for your own custom dirs
   config.load_paths += %W( 
     #{RAILS_ROOT}/app/sweepers
   )
 
+  # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem "RedCloth"
+
+  # Only load the plugins named here, in the order given (default is alphabetical).
+  # :all can be used as a placeholder for all plugins not explicitly named
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+
+  # Skip frameworks you're not going to use. To use Rails without a database,
+  # you must remove the Active Record framework.
+  config.frameworks -= [ :active_resource ]
+
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
+
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+
+  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names.
+  config.time_zone = 'UTC'
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
@@ -41,30 +58,9 @@ Rails::Initializer.run do |config|
   # like if you have constraints or database-specific column types
   config.active_record.schema_format = :ruby
 
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector
-
-  # Make Active Record use UTC-base instead of local time
-  # config.active_record.default_timezone = :utc
-  
-  # See Rails::Configuration for more options
-
+  # make actions work even if there's an error sending emails, that way a page will still load for the user
   config.action_mailer.raise_delivery_errors = false
   
-  # Load Engines first
-  #config.plugins = [:engines, :engines_model_mixins, :questionnaire_engine, :reference_engine, :all]
-  config.plugins = [:questionnaire_engine, :reference_engine, :all]
-
-  # shhhh secret!  apparently this is required in 2.x
-  config.action_controller.session = { :secret => "In the beginning was the Word, and the Word was with God, and the Word was God." }
-
-  # Some people want plugins not be be reloadable.  I disagree and definitely want plugins
-  # to be reloadable since we use them for code sharing.
-  #
-  # http://weblog.techno-weenie.net/2007/1/26/understanding-the-rails-plugin-initialization-process
-  # http://www.ruby-forum.com/topic/134860#600630
-  # http://dev.rubyonrails.org/ticket/5852
+  # since we use plugins for code sharing, we want to reload plugins code
   config.reload_plugins = true
 end
-
-ActiveRecord::Base.default_timezone = :utc # Store all times in the db in UTC
