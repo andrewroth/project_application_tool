dbdde4b73f8175beddf7caef09b32038df96c237
Andrew Roth <andrewroth@gmail.com>
2009-04-07 17:08:55 -0400

    use rails 2.2 turnk

--- lib/mysql_retry_lost_connection.rb
+++ lib/mysql_retry_lost_connection.rb
@@ -1,18 +1,25 @@
 module ActiveRecord
+  class NilResultError < ActiveRecordError
+  end
+
   module ConnectionAdapters
     class MysqlAdapter
       RETRY_COUNT = 3
+
+      def log_error(m, e, r)
+        RAILS_DEFAULT_LOGGER.info("MysqlAdapter FAIL: #{m} got #{e.class.name}.  Retries left: #{r}")
+      end
 
       # retry lost connection / server gone away errors
       def execute_with_handle_connection_errors(sql, name = nil) #:nodoc:
         retries = RETRY_COUNT
         begin
           execute_without_handle_connection_errors sql, name
-        rescue ActiveRecord::StatementInvalid => exception
-          RAILS_DEFAULT_LOGGER.info("MysqlAdapter FAIL: execute got StatementInvalid.  Retries left: #{retries}")
-          if retries > 0 and exception.message =~ /(Lost connection to MySQL server during query|MySQL server has gone away)/
+        rescue ActiveRecord::StatementInvalid, ActiveRecord::NilResultError => exception
+          retries -= 1
+          log_error('execute', exception, retries)
+          if retries > 0 and exception.message =~ /(Lost connection to MySQL server during query|MySQL server has gone away)|execute returned nil result/
             reconnect!
-            retries -= 1
             retry
           end
         end
@@ -20,20 +27,38 @@ module ActiveRecord
       alias_method_chain :execute, :handle_connection_errors
 
       # retry column methods that get nil back from execute
-      def columns_with_handle_executions(table_name, name = nil) #:nodoc:
+      def columns_with_handle_nils(table_name, name = nil) #:nodoc:
         retries = RETRY_COUNT
         begin
-          columns_without_handle_executions table_name, name
+          columns_without_handle_nils table_name, name
         rescue NoMethodError => exception
-          RAILS_DEFAULT_LOGGER.info("MysqlAdapter FAIL: column NoMethodError.  Retries left: #{retries}")
-          if retries > 0 and  exception.message =~ /(You have a nil object when you didn't expect it!)/
+          retries -= 1
+          log_error('column', exception, retries)
+          if retries > 0 and exception.message =~ /nil/
             reconnect!
-            retries -= 1
             retry
           end
         end
       end
-      alias_method_chain :columns, :handle_executions
+      alias_method_chain :columns, :handle_nils
+
+      # retry select methods that get nil back from execute
+      def select_with_handle_exceptions(table_name, name = nil) #:nodoc:
+        retries = RETRY_COUNT
+        begin
+          result = select_without_handle_exceptions table_name, name
+          raise ActiveRecord::NilResultError.new('select returned nil result') unless result
+          return result
+        rescue NoMethodError, ActiveRecord::NilResultError => exception
+          retries -= 1
+          log_error('select', exception, retries)
+          if retries > 0 && /undefined method `all_hashes' for nil:NilClass|select returned nil result/
+            reconnect!
+            retry
+          end
+        end
+      end
+      alias_method_chain :select, :handle_exceptions
 
     end
   end
