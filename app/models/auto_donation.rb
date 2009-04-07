ca73b0127768db5f877f6076be3fe46f42bb88bd
Andrew Roth <andrewroth@gmail.com>
2009-04-06 23:40:07 -0400

    remove DonorDirct donations out of orig_amount totals; add type, totals in for participants

--- app/models/auto_donation.rb
+++ app/models/auto_donation.rb
@@ -3,4 +3,11 @@ class AutoDonation < ActiveRecord::Base
   
   def motivation_code() participant_motv_code end
   def status() '' end
+
+  # remove all traces of original_amount, since it's always the same^M
+  def original_amount() nil end
+  def [](v)
+    return nil if v.to_sym == :original_amount
+    super
+  end
 end
