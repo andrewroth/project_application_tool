e9fc8f862d9e2183ff345dde71abf4cd55191230
Andrew Roth <andrewroth@gmail.com>
2009-04-08 14:59:06 -0400

    add require_dependency to reference_engine

--- app/models/reference_mailer.rb
+++ app/models/reference_mailer.rb
@@ -1,3 +1,5 @@
+require_dependency 'vendor/plugins/reference_engine/app/models/reference_mailer.rb'
+
 class ReferenceMailer < ActionMailer::Base
   def get_text_for_body(type)
     @appln.form.event_group.reference_emails.find_by_email_type(type).text
