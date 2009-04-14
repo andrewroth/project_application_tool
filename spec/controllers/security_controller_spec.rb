9879915bf675c1ea629cfde2f78606c0fecd3926
Andrew Roth <andrewroth@gmail.com>
2009-04-14 10:48:52 -0400

    make cas think it's already gone to the cas server to avoid redirect

--- spec/controllers/security_controller_spec.rb
+++ spec/controllers/security_controller_spec.rb
@@ -4,6 +4,7 @@ describe SecurityController do
   integrate_views
   
   before do 
+    session[:cas_sent_to_gateway] = true # make cas think it's already gone to the server to avoid redirect
     @viewer = mock_model(Viewer, :id => 1, :viewer_userID => "copter", :viewer_passWord => "9cdfb439c7876e703e307864c9167a15")
     Viewer.stub!(:find).and_return(@viewer)
     @params = { :username => "copter", :password => "lol"}
@@ -15,7 +16,7 @@ describe SecurityController do
   end
   
   it "should login a user with cim login" do
-    post :login, :viewer =>@params
+    post :login, @params
     puts "r: #{response.body}"
     assigns[:viewer].should == @viewer
   end
