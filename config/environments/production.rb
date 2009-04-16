9acb42c5ee1963820b0af0c0a0657b325bf5b952
Andrew Roth <andrewroth@gmail.com>
2009-04-16 18:47:56 -0400

    try a fresh production environment, mainly to get threadsafe! on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,26 +4,32 @@
 # Code is not reloaded between requests
 config.cache_classes = true
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
 
+# See everything in the log (default is :info)
+# config.log_level = :debug
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
+# config.action_controller.asset_host = "http://assets.example.com"
 
-# Disable delivery errors if you bad email addresses should just be ignored
+# Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+# Enable threaded mode
+config.threadsafe!
+
+# Mail settings
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.powertochange.local',
   :domain => 'powertochange.local'
 }
-
-# eager loading messes up active scaffold and a bunch of other things
-config.eager_load_paths = []
-
