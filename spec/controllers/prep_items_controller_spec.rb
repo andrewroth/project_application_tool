36210ec1131295c2755aab0ac5766e396ceb3ec5
Evan Chan <evanofthechans@gmail.com>
2009-04-16 10:41:43 -0400

    prep controller spec, login working

--- spec/controllers/prep_items_controller_spec.rb
+++ spec/controllers/prep_items_controller_spec.rb
@@ -8,10 +8,14 @@ describe PrepItemsController do
    before do 
     session[:cas_sent_to_gateway] = true # make cas think it's already gone to the server to avoid redirect
     @viewer = mock_model(Viewer, :id => 1, :viewer_userID => "copter", :viewer_passWord => "9cdfb439c7876e703e307864c9167a15",
-                        :viewer_isActive= => 1, :viewer_lastLogin= => Time.now, :save! =>'', :person =>'')
+                        :viewer_isActive= => 1, :viewer_lastLogin= => Time.now, :save! =>'', :person =>'', :is_student? => false)
     Viewer.stub!(:find).and_return(@viewer)
+    @event_group = mock_model(EventGroup, :id => 1, :empty? => false, :logo => "a", :projects=>'')
+    EventGroup.stub!(:find).and_return(@event_group)
+    @project = mock_model(Project, :id => 1, :hidden => false)
+    Project.stub!(:find).and_return(@project)
     session[:user_id] = @viewer.id
-  
+    session[:event_group_id]=1
   end
   
   it "should redirect to index on successful save" do
