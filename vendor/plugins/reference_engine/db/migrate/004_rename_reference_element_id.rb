7742c3d44e9c73cc15dabceba8469c00ddbd3591
Andrew Roth <andrewroth@gmail.com>
2009-07-20 02:18:21 -0400

    rename migrations for date format

--- vendor/plugins/reference_engine/db/migrate/004_rename_reference_element_id.rb
+++
@@ -1,9 +0,0 @@
-class RenameReferenceElementId < ActiveRecord::Migration
-  def self.up
-    rename_column ReferenceInstance.table_name, :element_id, :reference_id
-  end
-
-  def self.down
-    rename_column ReferenceInstance.table_name, :reference_id, :element_id
-  end
-end
