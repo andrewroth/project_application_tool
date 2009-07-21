b6816d084ecd8a55476d6da1277277e732edc819
Andrew Roth <andrewroth@gmail.com>
2009-07-21 02:28:18 -0400

    didn't mean to merge moonshine yet

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,90 @@
-server "192.168.1.116", :app, :web, :db, :primary => true
-#set :branch, 'moonshine'
+require 'find'
+require 'etc'
+
+ENV['target'] ||= 'dev'
+ENV['system'] ||= 'p2c'
+
+ENV['user'] = 'deploy'
+
+set :application, "Project Application Tool"
+set :scm, "git"
+set :repository,  "git://github.com/andrewroth/project_application_tool.git"
+
+if %w(ma mh).include? ENV['system']
+  ENV['host'] ||= 'ministryapp.com'
+  ENV['domain'] ||= 'pat.ministryapp.com'
+  ENV['port'] ||= '40022'
+elsif %w(p2c pc).include? ENV['system']
+  ENV['host'] ||= 'pat2.powertochange.org'
+  ENV['domain'] ||= 'pat.powertochange.org'
+  ENV['port'] ||= '22'
+end
+
+ENV['deploy_to'] ||= "/var/www/#{if ENV['target'] == 'prod' then 
+                       ENV['domain'] else "#{ENV['target']}.#{ENV['domain']}" end}"
+
+if ENV['env']
+  RAILS_ENV = ENV['env']
+elsif ENV['target'] == 'dev'
+  RAILS_ENV = 'development'
+elsif ENV['target'] == 'demo'
+  RAILS_ENV = 'production'
+elsif ENV['target'] == 'prod'
+  RAILS_ENV = 'production'
+
+end
+
+puts
+puts "host = #{ENV['host']}:#{ENV['port']}"
+puts "user = #{ENV['user']}"
+puts "env = #{RAILS_ENV}"
+puts "deploy_to = #{ENV['deploy_to']}"
+puts
+
+role :app, ENV['host']
+role :web, ENV['host']
+role :db,  ENV['host'], :primary => true
+
+ssh_options[:port] = ENV['port']
+set :user, ENV['user']
+
+set :deploy_to, ENV['deploy_to']
+
+# remove old deploys after each new deploy
+set :use_sudo, false
+after "deploy", "deploy:cleanup"
+
+desc "Restart the web server"
+deploy.task :restart, :roles => :app do
+  # sudo "/opt/lsws/bin/lswsctrl restart"
+  run "touch #{current_path}/tmp/restart.txt"
+end
+
+deploy.task :before_migrate do
+  run "cd #{current_path}; RAILS_ENV=#{RAILS_ENV} rake db:setup:pat"
+end
+
+def link_shared(p, o = {})
+  if o[:overwrite]
+    run "rm -Rf #{release_path}/#{p}"
+  end
+
+  run "ln -s #{shared_path}/#{p} #{release_path}/#{p}"
+end
+
+unless ENV['target'] == 'demo'
+  deploy.task :after_symlink do
+    # set up tmp dir
+    run "mkdir -p -m 770 #{shared_path}/tmp/{cache,sessions,sockets,pids}"
+    run "rm -Rf #{release_path}/tmp"
+    link_shared 'tmp'
+
+    # other shared files / folders
+    link_shared 'log', :overwrite => true
+    link_shared 'config/database.yml'
+    link_shared 'public/event_groups'
+    if RAILS_ENV == 'development'
+      link_shared 'config/environments/development.rb', :overwrite => true
+    end
+  end
+end
