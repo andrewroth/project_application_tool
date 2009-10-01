4f0d96a08ac11dd9741550e6abacf4ca24209ad9
Andrew Roth <andrewroth@gmail.com>
2009-10-01 17:02:35 -0400

    fix issue creating elements with flags by default

--- vendor/plugins/questionnaire_engine/lib/flags.rb
+++ vendor/plugins/questionnaire_engine/lib/flags.rb
@@ -1,18 +1,37 @@
 module Flags
 
+  def respond_to?(signal, includePriv=false)
+    signal.to_s.scan(/(is_)?(\w+)[=|\?]?/) do |flag_str|
+      flag_str = flag_str.second
+      return true if Flag.find(:first, :conditions => { :name => flag_str })
+    end
+    return super
+  end
+
   def method_missing(*params)
     m = params[0].to_s
     
     # try to assign or read flags
     m.scan(/(is_)?(\w+)[=|\?]?/) do |flag_str|
-      flag_str = flag_str[1] # for some reason the string come as a single sized array
+      flag_str = flag_str.second
       return super unless Flag.find(:first, :conditions => { :name => flag_str })
 
       begin
         if m.last_char == '='
-          return set_flag_value(flag_str, params[1])
+          if self.new_record?
+            @initial_flags ||= {} 
+            @initial_flags[m] = params[1]
+            return true
+          else
+            return set_flag_value(flag_str, params[1])
+          end
         else
-          return get_value_of(flag_str)
+          if self.new_record?
+            @initial_flags ||= {} 
+            return @initial_flags[params[1]]
+          else
+            return get_value_of(flag_str)
+          end 
         end
       rescue
         super
@@ -22,16 +41,23 @@ module Flags
     super
   end
   
+  attr_reader :initial_flags
+
+  def set_initial_flags
+    (@initial_flags || {}).each { |k,v| puts "#{k} #{v}"; self.send(k, v) }
+  end
+
   def self.setup(*params)
     params = params[0]
     base = params[:self]
     
     base.class_eval do
-      c = "define_method('flag_values_model') do #{params[:flag_values_model]} end"
+      c = "define_method('flag_values_model') do #{params[:flag_values_model]} end; "
 #      puts c
       eval c
       
       has_many :flag_values, :class_name => params[:flag_values_model].to_s
+      after_create :set_initial_flags
     end
   end
   
