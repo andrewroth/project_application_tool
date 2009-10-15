744da290b1586503146788d5d1af5d5cfb269b23
Andrew Roth <andrewroth@gmail.com>
2009-10-15 01:19:47 -0400

    expire cache when ref submitted

--- app/controllers/reference_instances_controller.rb
+++ app/controllers/reference_instances_controller.rb
@@ -5,15 +5,19 @@ class ReferenceInstancesController < InstanceController
   include Permissions
   include AcceptancePileFunctionality
 
-  before_filter :set_profile, :only => BYPASS_ACTIONS
+  before_filter :set_profile, :only => BYPASS_ACTIONS + [ :submit ]
   before_filter :ensure_profile_processor, :only => BYPASS_ACTIONS
   before_filter :clear_user, :except => BYPASS_ACTIONS
+
+  after_filter :sweep, :only => :submit
   
   skip_before_filter :verify_user, :restrict_students
   skip_before_filter :verify_event_group_chosen
   skip_before_filter :set_event_group
   skip_before_filter :set_filter
   skip_before_filter :get_pages, :only => [ :no_access ]
+
+  cache_sweeper :profiles_sweeper
 
   protected
 
@@ -27,5 +31,9 @@ class ReferenceInstancesController < InstanceController
   
   def set_profile
     @profile = @reference_instance.instance.profile
+  end
+
+  def sweep
+    expire_fragment(%r{your_projects.project_id=#{@profile[:project_id]}&role=.*&section=Submitted})
   end
 end 
