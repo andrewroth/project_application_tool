289ee711e6f9859399c71624e6740cfdbd3823c9
Andrew Roth <andrewroth@gmail.com>
2009-10-15 01:18:13 -0400

    use filestore in dev for easier debugging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ config.cache_classes = false
 config.whiny_nils = true
 
 # Show full error reports and disable caching
+config.cache_store = :file_store, 'tmp/cache'
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
 config.action_view.debug_rjs                         = true
