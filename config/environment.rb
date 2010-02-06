b6b1b0d69e765f840cc1696468a0c544f6bf35d2
Andrew Roth <andrewroth@gmail.com>
2010-02-06 13:19:59 -0500

    upgrade to rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2'
+RAILS_GEM_VERSION = '2.3.5'
 
 require 'rubygems'
 
