85826b1ecf894674f869cfac1fdfe02ff840768a
Andrew Roth <andrewroth@gmail.com>
2010-03-09 15:44:29 -0500

    clear cache on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,6 +83,8 @@ unless ENV['target'] == 'demo'
       link_shared 'config/environments/development.rb', :overwrite => true
     end
     # TODO: test ln's for pat2. www.pat and www.pat2
-    #run "ln -s #{release_path}/tmp/cache/views/pat.powertochange.org #{release_path}/tmp/cache/views/pat2.powertochange.org"
+    run "rm -Rf #{release_path}/tmp/cache/*"
+    run "mkdir -p #{release_path}/tmp/cache/views/pat.powertochange.org"
+    run "ln -s #{release_path}/tmp/cache/views/pat.powertochange.org #{release_path}/tmp/cache/views/pat2.powertochange.org"
   end
 end
