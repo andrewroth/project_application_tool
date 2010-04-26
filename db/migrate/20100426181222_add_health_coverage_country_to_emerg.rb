e67ab405b6eaa600941646f7459bcf88c5968171
Andrew Roth <andrewroth@gmail.com>
2010-04-26 15:47:45 -0400

    remove migration for country emerg since that info is derived from the province row

--- db/migrate/20100426181222_add_health_coverage_country_to_emerg.rb
+++
@@ -1,9 +0,0 @@
-class AddHealthCoverageCountryToEmerg < ActiveRecord::Migration
-  def self.up
-    add_column :emergs, :health_coverage_country, :string
-  end
-
-  def self.down
-    remove_column :emergs, :health_coverage_country
-  end
-end
