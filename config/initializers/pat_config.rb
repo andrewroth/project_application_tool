946b53dcb152203e9ac587d5c06748756ea8c13b
Andrew Roth <andrewroth@gmail.com>
2011-01-13 16:31:25 -0500

    we have cell phones

--- config/initializers/pat_config.rb
+++ config/initializers/pat_config.rb
@@ -1,7 +1,7 @@
 class Pat
   CONFIG = {
     :person_has_title => false,
-    :person_has_cell_phone => false,
+    :person_has_cell_phone => true,
     :emerg_contact_cdn_style => true,
     :gender_is_lookup_table => true
   }
