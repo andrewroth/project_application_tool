439e766bd900705f1960862705a462bbc6f5e9fb
Andrew Roth <andrewroth@gmail.com>
2011-06-24 13:26:04 -0400

    add title

--- config/initializers/pat_config.rb
+++ config/initializers/pat_config.rb
@@ -3,6 +3,7 @@ class Pat
     :person_has_title => false,
     :person_has_cell_phone => false,
     :emerg_contact_cdn_style => false,
-    :gender_is_lookup_table => false
+    :gender_is_lookup_table => false,
+    :title => "Project Application Tool"
   }
 end
