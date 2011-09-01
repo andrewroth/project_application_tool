9883e06e62a26a82fc57742bd620ab9b35955a1b
Andrew Roth <andrewroth@gmail.com>
2011-09-01 13:40:20 -0400

    handle case where element is nil

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -27,11 +27,12 @@ module ElementsHelper
 
   def custom_element_visible(name, method = "header")
     return true if custom_element_edit_mode || custom_element_create_mode
+    return true if @element.nil?
     !@element.custom_element_hidden_sections.find_by_name(name, method).present?
   end
 
   def custom_element_item(txt, m, c)
-    if @element.custom_element_hidden_sections.detect{ |s| s.name == m.to_s && s.attribute == c.to_s }
+    if @element.nil? || @element.custom_element_hidden_sections.detect{ |s| s.name == m.to_s && s.attribute == c.to_s }
       return ""
     end
 
