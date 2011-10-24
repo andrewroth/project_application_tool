00176c311208db614433eebaa0bd53400555f48b
Andrew Roth <andrewroth@gmail.com>
2011-10-24 17:54:35 -0400

    update to use the key

--- config/initializers/cas.rb
+++ config/initializers/cas.rb
@@ -3,11 +3,14 @@ cas_logger = CASClient::Logger.new(RAILS_ROOT+'/log/cas.log')
 cas_logger.level = Logger::DEBUG
 
 CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url  => "https://signin.mygcx.org/cas/",
+  :cas_base_url  => "https://thekey.me/cas/",
+  :login_url     => "https://thekey.me/cas/login",
+  :logout_url    => "https://thekey.me/cas/logout",
+  :validate_url  => "https://thekey.me/cas/proxyValidate",    
+  :proxy_retrieval_url => "https://pat.powertochange.org/cas_proxy_callback/retrieve_pgt",
+  :proxy_callback_url => "https://pat.powertochange.org/cas_proxy_callback/receive_pgt",
   :username_session_key => :cas_user,
   :extra_attributes_session_key => :cas_extra_attributes,
-  :proxy_retrieval_url => "https://www.globalshortfilmnetwork.com/cas_proxy_callback/retrieve_pgt",
-  :proxy_callback_url => "https://www.globalshortfilmnetwork.com/cas_proxy_callback/receive_pgt",
-  # :authenticate_on_every_request => true,
-  :logger => cas_logger
+  :logger => cas_logger,
+  :authenticate_on_every_request => false
 )
