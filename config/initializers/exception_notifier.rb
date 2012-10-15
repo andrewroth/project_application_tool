a80bbd68e8040c93fffe92962da8d7b31aa2d44d
Andrew Roth <andrewroth@gmail.com>
2012-10-15 14:13:47 -0400

    use my p2c email

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,2 @@
-ExceptionNotifier.exception_recipients = %w(andrew.roth@c4c.ca)
+ExceptionNotifier.exception_recipients = %w(andrew.roth@powertochange.org)
 ExceptionNotifier.sender_address = %w(pat.help@powertochange.org)
