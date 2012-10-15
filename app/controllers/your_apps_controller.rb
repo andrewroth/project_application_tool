6b3a913432fec8acc0c7f1329d8abb5fc613fe3e
Andrew Roth <andrewroth@gmail.com>
2012-10-15 14:02:09 -0400

    fix starting new projects

--- app/controllers/your_apps_controller.rb
+++ app/controllers/your_apps_controller.rb
@@ -16,15 +16,20 @@ class YourAppsController < ApplicationController
 
   def view
     profiles = @viewer.profiles.find_all{ |profile| profile.try(:project).try(:event_group_id) == @eg.id || profile.try(:appln).try(:form).try(:event_group_id) == @eg.id }
-    if profiles.length != 1
+    if profiles.length > 1
       flash[:notice] = "You have multiple applications - please choose one to view."
       redirect_to :controller => :profiles, :action => :index
-    else
-      if profiles.first.is_a?(Acceptance)
-        redirect_to({ :controller => 'appln', :action => 'view_always_editable', :profile_id => profiles.first.id })
+    elsif profiles.first.is_a?(Acceptance)
+      redirect_to({ :controller => 'appln', :action => 'view_always_editable', :profile_id => profiles.first.id })
+    elsif profiles.empty?
+      if @eg.forms.find_all_by_hidden(false).count == 1
+        redirect_to :controller => :profiles, :action => :start, :form_id => @eg.forms.find_all_by_hidden(false).first
       else
-        redirect_to :controller => :appln, :profile_id => profiles.first.id
+        # multiple forms, they need to choose
+        redirect_to :controller => :profiles, :action => :index
       end
+    else
+      redirect_to :controller => :appln, :profile_id => profiles.first.id
     end
   end
 
