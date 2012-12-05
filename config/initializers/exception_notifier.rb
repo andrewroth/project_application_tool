5ea5e69bb4e43b469c308e9d7826735e907b4893
Andrew Roth <andrewroth@gmail.com>
2012-12-05 18:51:02 -0500

    add pat.help@powertochange.com to get crash reports

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,2 @@
-ExceptionNotifier.exception_recipients = %w(andrew.roth@powertochange.org)
+ExceptionNotifier.exception_recipients = %w(andrew.roth@powertochange.org pat.help@powertochange.org)
 ExceptionNotifier.sender_address = %w(pat.help@powertochange.org)
