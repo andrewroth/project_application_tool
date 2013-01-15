803ed010c545f30a83f8b9e609081f9fcac3998a
Andrew Roth <andrewroth@gmail.com>
2013-01-15 15:12:06 -0500

    use my personal email for pat crashes

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,2 @@
-ExceptionNotifier.exception_recipients = %w(andrew.roth@powertochange.org pat.help@powertochange.org)
+ExceptionNotifier.exception_recipients = %w(andrewroth@gmail.com pat.help@powertochange.org)
 ExceptionNotifier.sender_address = %w(pat.help@powertochange.org)
